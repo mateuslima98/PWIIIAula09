c9bb7a1c40813d96f232701ab8df2fed1ae078b1
Mateus98 <mateus_lima98@hotmail.com>
2019-09-20 22:30:22 -0300

    Aplicação de Animação

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'has-content',
@@ -10,6 +11,20 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const options = {
+      stringsElement: '#typed-strings',
+      strings: ['ETEC - Banco Digital','Peça a portabilidade e venha ser ETEC hoje mesmo','Inovavor, digital e seguro'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 200,
+      smartBackspace: true,
+      fadeOut: true,
+      showCursor: false,
+      startDelay: 1000,
+      loop: true
+    };
+	
+	const typed = new Typed('.typing-element', options);
   }
-
 }
+ 
